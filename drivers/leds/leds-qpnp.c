6cf04d80d1d39f944e6dda549a704cd0351c013a
Haikal Izzuddin <heywhite69@gmail.com>
2018-08-29 10:30:18 +0800

    Update leds-qpnp.c

--- drivers/leds/leds-qpnp.c
+++ drivers/leds/leds-qpnp.c
@@ -1719,18 +1719,6 @@ static int qpnp_rgb_set(struct qpnp_led_data *led)
 			led->rgb_cfg->pwm_cfg->mode =
 				led->rgb_cfg->pwm_cfg->default_mode;
 		if (led->rgb_cfg->pwm_cfg->mode == PWM_MODE) {
-<<<<<<< HEAD
-			pwm_disable(led->rgb_cfg->pwm_cfg->pwm_dev);
-			led->rgb_cfg->pwm_cfg->pwm_enabled = 0;
-					rc = qpnp_led_masked_write(led,
-			RGB_LED_EN_CTL(led->base),
-			led->rgb_cfg->enable, RGB_LED_DISABLE);
-
-
-			dev_err(&led->pdev->dev,
-					"zjl   pwm config 333 \n");
-=======
->>>>>>> stable/kernel.lnx.4.4.r35-rel
 			rc = pwm_change_mode(led->rgb_cfg->pwm_cfg->pwm_dev,
 					PM_PWM_MODE_PWM);
 			if (rc < 0) {
